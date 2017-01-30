72b30eb262cceb33021b9fb9972ad5b29da51b63
amir-cli <cernerteam8@gmail.com>
2017-01-30 16:19:59 +0330

    Update mypic.lua

--- plugins/mypic.lua
+++ plugins/mypic.lua
@@ -2,13 +2,13 @@ do
 function run(msg, matches)
 local reply_id = msg['id']
 
-local info = 'telegram.me/'..msg.from.username
+local info = 't.me/'..msg.from.username
 reply_msg(reply_id, info, ok_cb, false)
 end
 
 return {
 patterns = {
-"^[!/#](mypic)"
+"^(عکس من)"
 
 },
 run = run
